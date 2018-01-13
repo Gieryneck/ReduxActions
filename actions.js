e5f2e342137409709c9e773412ac1f3fdec16ddf
Gieryneck <gierynt@gmail.com>
2018-01-13 16:05:02 +0100

    did actions.js

---
+++ actions.js
@@ -0,0 +1,47 @@
+/* 
+Stwórz plik actions.js, w którym zdefiniujesz typy akcji oraz kreatory akcji dla następujących zdarzeń:
+
+tworzenie komentarza,
+edycja komentarza,
+usuwanie komentarza,
+ocenianie +1 / -1 (kciuk w górę/kciuk w dół) komentarza
+ */
+
+function addComment(text, userId, postId) {
+
+    return {
+        type: 'ADD_COMMENT',
+        text,
+        userId,
+        postId,
+        commentId: uuid.v4()    
+    }
+}  
+
+const addComment = (text, userId, postId) => dispatch(addComment(text, userId, postId));
+// addComment('Hey, that is an awesome picture!', User123, PostXYZ);
+
+
+
+
+function editComment(text, commentId) {
+
+    return {
+        type: 'EDIT_COMMENT',
+        text,
+        commentId,
+    }
+} 
+
+const editComment = (text, commentId) => dispatch(addComment(text, commentId));
+
+
+function deleteComment(commentId) {
+    
+    return {
+        type: 'DELETE_COMMENT',
+        commentId
+    }
+} 
+
+const deleteComment = (commentId) => dispatch(deleteComment(commentId));
